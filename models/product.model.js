f79d891b2a713554ed542d8b12f390da6c67ccd1
arunkumarayinabathina <arun987610@gmail.com>
2024-11-21 11:36:28 +0530

    initial commit

---
+++ models/product.model.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const ProductSchema = mongoose.Schema(
+    {
+        name:{
+            type:String,
+            required : [true,"Please enter product name"],
+        },
+        quantity :{
+            type:String,
+            required:true,
+            default :0
+        },
+        price:{
+            type:Number,
+            required:true,
+            default:0
+        }
+    },
+    {
+        timestamp:true
+    }
+);
+const Product = mongoose.model("Product", ProductSchema);
+module.exports = Product;
---
+++ server.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const product = require("./models/product.model.js");
+const productRoute = require("./routes/product.route.js");
+
+const app = express();
+PORT = 5000;
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+
+app.use("/api/products", productRoute);
+
+app.get('/',(req,res)=>{
+    res.send("Welcome to home page!");
+})
+
+const user_id=process.env.USER_ID;
+const password = process.env.PASS;
+mongoose
+  .connect(
+    `mongodb+srv://${user_id}:${password}@backend.fif3f.mongodb.net/NODE-api?retryWrites=true&w=majority&appName=Backend`
+  )
+  .then(() => {
+    console.log("Connected to database!");
+    app.listen(5000, () => {
+      console.log("Server is running on port 5000");
+    });
+  })
+  .catch(() => {
+    console.log("Connection failed!");
+  });
